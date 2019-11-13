2da4d251751e3781c96a5607c15054011d674722
jjfaulk1967 <sr.network.engineer@gmail.com>
2019-11-13 11:18:28 -0800

    Create python2.py

---
+++ Python/IPERF3/python2.py
@@ -0,0 +1,44 @@
+# Dynamic login to linux host to ping another linux host.
+
+# Define connection parameters
+
+from getpass import getpass
+from netmiko import ConnectHandler
+
+# Define dynamic login
+
+username = raw_input('Enter your SSH username: ')
+password = getpass()
+
+# Statically define host and dynamic credentials
+
+linux = {
+    'device_type': 'linux',
+    'ip': '192.168.122.76',
+    'username': username,
+    'password': password,
+}
+
+# Connect to linux host
+
+net_connect = ConnectHandler(**linux)
+
+# Define command
+
+command = "ping -c 5 192.168.122.82"
+
+# Print to screen the hostname
+
+print(net_connect.find_prompt())
+
+# Send command
+
+output = net_connect.send_command(command)
+
+# Disconnect ssh session
+
+net_connect.disconnect()
+
+# Print results of command
+
+print(output)
